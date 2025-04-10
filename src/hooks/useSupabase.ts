7bf95a7947eef96fd36067754e170ac1d2256771
MasterZensei <zhan.qureshi@gmail.com>
2025-04-11 01:37:06 +0300

    Remove unused nanoid import

--- src/hooks/useSupabase.ts
+++ src/hooks/useSupabase.ts
@@ -1,5 +1,4 @@
 import { useCallback, useEffect, useState } from 'react';
-import { nanoid } from 'nanoid';
 import { supabase, GameSettings, GameState, Player, supabaseUrl, supabaseAnonKey } from '../lib/supabaseClient';
 
 // Hook for managing lobbies
